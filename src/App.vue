7d8dbdd89a4e34f2b747548d343320d8a05a9d18
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-26 15:15:51 +0300

    structures App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <script setup>
   import StepProgress from './components/StepProgress.vue';
 
-  const datasteps = {
+  const dataSteps = {
     steps: ['Step 1', 'Step 2', 'Step 3', 'Step 4'],
     currentStep: 1,
     activeColor: 'bg-blue-500',
     inactiveColor: 'bg-gray-200',
   }
+
 </script>
 
 <template>
   <div>
-    <StepProgress :data="datasteps" />
+    <StepProgress :data="dataSteps" />
   </div>
 </template>
