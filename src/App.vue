c4f19c75c08cfbb096492480b0adb635f16d32e0
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-27 12:25:40 +0300

    reformats frontend files

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <script setup>
-  import StepProgress from './components/StepProgress.vue';
-  import { ref } from 'vue';
+import StepProgress from "./components/StepProgress.vue";
+import { ref } from "vue";
 
-  const stepProgress = ref()
+const stepProgress = ref();
 
-  const dataSteps = {
-    steps: ['Step 1', 'Step 2', 'Step 3', 'Step 4'],
-    currentStep: 1,
-    activeColor: '#3b82f6',
-    passiveColor: '#d1d5db',
-  }
-
+const dataSteps = {
+  steps: ["Step 1", "Step 2", "Step 3", "Step 4"],
+  currentStep: 1,
+  activeColor: "#3b82f6",
+  passiveColor: "#d1d5db",
+};
 </script>
 
 <template>
-    <StepProgress :data="dataSteps" ref="stepProgress" />
-    <div class="">
-      <button @click="stepProgress.previousStep">Previous Step</button>
-      <button @click="stepProgress.nextStep">Next Step</button>
-
-      </div>
+  <StepProgress :data="dataSteps" ref="stepProgress" />
+  <div class="">
+    <button @click="stepProgress.previousStep">Previous Step</button>
+    <button @click="stepProgress.nextStep">Next Step</button>
+  </div>
 </template>
