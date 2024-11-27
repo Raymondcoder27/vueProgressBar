dda98fbbd8ac13c25a015adccaf97ede39f8d427
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-27 11:44:08 +0300

    reformats frontend files

--- src/components/StepProgress.vue
+++ src/components/StepProgress.vue
@@ -2,38 +2,32 @@
 import { defineProps, ref, computed } from "vue";
 
 const props = defineProps({
-  data: Object,
+  data: {
+    type: Object,
+    required: true,
+  },
 });
 
-// props.data.currentStep--;
+// Reactive data copy
+const data = ref({ ...props.data, steps: props.data.steps || [] });
 
-// const data = ref(props.data);
-// const data = ref({ ...props.data });
-const data = ref({
-  ...props.data,
-  steps: props.data.steps || [], // Ensure steps is defined
-});
+// Adjust currentStep only if valid
+if (data.value.currentStep > 0) {
+  data.value.currentStep--;
+}
 
-// props.data.currentStep--;
-
-data.value.currentStep--; // Safer within the reactive state
-
-const cssStyle = computed(() => {
-  return {
-    "--active-color": data.value.activeColor,
-    "--passive-color": data.value.passiveColor,
-  };
-});
+const cssStyle = computed(() => ({
+  "--active-color": data.value.activeColor || "#3b82f6",
+  "--passive-color": data.value.passiveColor || "#d1d5db",
+}));
 </script>
 
 
 <template>
   <div class="steps-container" :style="cssStyle">
     <ul class="steps-list">
-      <!-- <li class="step" v-for="(step, index) in data.value.steps" :key="index" :class="(index == data.currentStep )? '--stepActive' : ''"> -->
-      <!-- <li v-for="(step, index) in data.value.steps" :key="index" :class="(index == data.value.currentStep) ? '--stepActive' : ''"> -->
       <li
-        v-for="(step, index) in data.value.steps || []"
+        v-for="(step, index) in data.value.steps"
         :key="index"
         :class="{ '--stepActive': index === data.value.currentStep }"
       >
@@ -45,53 +39,3 @@ const cssStyle = computed(() => {
     </ul>
   </div>
 </template>
-
-<style scoped>
-.steps-container {
-  width: 100%;
-  margin: 0 auto;
-  margin-left: 500px;
-}
-
-.steps-list {
-  display: flex;
-  list-style: none;
-}
-
-.step:last-child {
-  max-width: 60px;
-}
-.step {
-  display: flex;
-  align-items: center;
-  flex-grow: 1;
-  max-width: 100%;
-  position: absolute;
-  height: 60px;
-}
-
-.step-bubble {
-  width: 35px;
-  height: 35px;
-  border-radius: 50%;
-  background-color: var(--passive-color);
-  transition: all 0.3s ease;
-  display: flex;
-  justify-content: center;
-}
-
-.step-line {
-  width: 100%;
-  height: 5px;
-  background-color: var(--passive-color);
-  position: absolute;
-  top: 50%;
-  left: 0;
-  transform: translate(-50%);
-  z-index: -10;
-}
-
-.step:last-child .step-line {
-  display: none;
-}
-</style>
