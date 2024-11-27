99865df17f7b527d322f79f799bae04c0415850d
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-27 12:39:48 +0300

    fixes posts call after post addition

--- src/components/icons/IconDone.vue
+++ src/components/icons/IconDone.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-<svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<svg class="step-done-icon" width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M4 12.6111L8.92308 17.5L20 6.5" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
 </template>
