8cbe0b527496b0ff04692ed4e9d7baf52aa479db
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-27 11:03:04 +0300

    reformats frontend files

--- src/components/StepProgress.vue
+++ src/components/StepProgress.vue
@@ -55,5 +55,17 @@ const cssStyle = computed(() => {
     height: 35px;
     border-radius: 50%;
     background-color: var(--passive-color);
+    transition:all 0.3s ease;
+     display: flex;
+    justify-content: center;
+  }
+
+  .step-line{
+    width: 100%;
+    height: 5px;
+    background-color: var(--passive-color);
+    top: 50%;
+    left: 0;
+    transform: translate(-50%)
   }
 </style>
